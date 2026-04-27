27bc1c2ca777d4317301883f97fe189f18eb7873
ravish <ravishkumar9088@.com>
2026-04-27 19:01:14 +0530

    checking api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 console.log("ENV KEY:", process.env.OPENROUTER_API_KEY);
-require('dotenv').config();
+require('dotenv').config({ quiet: true });
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
